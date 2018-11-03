1d41f836255334eeca7832f056ee7cfc12221263
Greg Morgan <gregm9999@gmail.com>
2018-11-03 20:44:39 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+require 'pry'
+collection=[1,2,3,4]
+def my_each(array)# put argument(s) here
   # code here
+  # binding.pry
+  i=0
+  while i <array.length
+    yield array[i]
+    i+=1
+  end
+  array
+  # binding.pry
+end
+
+my_each(collection) do |i|
+   i
 end
